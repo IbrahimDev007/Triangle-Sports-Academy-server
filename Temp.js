4ac64aa7a48c52e1603b36c791a045786e3a11be
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-06-18 20:05:45 +0600

    mongodb uri

--- Temp.js
+++ Temp.js
@@ -32,7 +32,8 @@ const verifyJWT = (req, res, next) => {
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { Console } = require('console');
 
-const uri = `mongodb://127.0.0.1:27017`;
+// const uri = `mongodb://127.0.0.1:27017`;
+const uri = process.env.URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
